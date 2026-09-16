ac6011247ba1bf15f109e9ee93ff431a02b0e8eb
Ctoagn1 <qwezese@gmail.com>
2026-09-16 02:01:36 -0400

    added lerp

--- scripts/player.gd
+++ scripts/player.gd
@@ -6,10 +6,25 @@ var parent_node
 
 var moved = false;
 
+var t = 0;
+var in_motion = false;
+var newpos = Vector2i(0, 0)
+var oldpos = global_position
+
 func _ready() -> void:
 	global_position = TILEMAP.map_to_local(Vector2i(0, 0))
 	parent_node = get_parent()
+
 	
+func _physics_process(delta: float) -> void:
+	if(in_motion):
+		t += delta *7.5
+		global_position = oldpos.lerp(newpos, t)
+		if(t >= 1):
+			in_motion = false;
+			oldpos = newpos
+			t = 0;
+		
 func _process(_delta: float) -> void:
 
 	if(parent_node.is_on_beat() && !moved):
@@ -37,7 +52,10 @@ func _process(_delta: float) -> void:
 			if parent_node.in_bounds(pos + Vector2i(0, 1)):
 				pos = pos + Vector2i(0, 1)
 				moved = true
-		global_position = TILEMAP.map_to_local(pos)
+		if (moved):	
+			in_motion = true;
+			oldpos = global_position
+			newpos = TILEMAP.map_to_local(pos)
 	else:
 		if Input.is_action_pressed("Down") || Input.is_action_pressed("Up") || Input.is_action_pressed("Left") || Input.is_action_pressed("Right"):
 			print_debug("boo you suck at timing")
